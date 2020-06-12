65024333b6cda82ffc77a67534a81e8085d5634b
JosephineAkello <josephineakello2030@gmail.com>
2020-06-12 22:40:57 +0300

    add enumerated annotation

--- src/main/java/com/example/recipeproject/domain/Recipe.java
+++ src/main/java/com/example/recipeproject/domain/Recipe.java
@@ -3,6 +3,8 @@ package com.example.recipeproject.domain;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,6 +33,9 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -109,5 +114,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
