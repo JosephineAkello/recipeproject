f8e181cce6fe42609623162d508250d54d679621
JosephineAkello <josephineakello2030@gmail.com>
2020-06-16 15:53:30 +0300

    place cat and unit of measure repositories

--- src/main/java/com/example/recipeproject/controllers/IndexControllers.java
+++ src/main/java/com/example/recipeproject/controllers/IndexControllers.java
@@ -1,14 +1,35 @@
 package com.example.recipeproject.controllers;
 
 
+import java.util.Optional;
+
+import com.example.recipeproject.domain.Category;
+import com.example.recipeproject.domain.UnitofMeasure;
+import com.example.recipeproject.repositories.CategoryRepository;
+import com.example.recipeproject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexControllers {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexControllers(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/","/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("America");
+        Optional<UnitofMeasure> unitofmeasureOptional = unitOfMeasureRepository.findByDescription("");
+
+        System.out.println("My cat ID is" + categoryOptional.get().getId());
+        System.out.println("My unit of measure ID is" + unitofmeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/example/recipeproject/repositories/CategoryRepository.java
+++ src/main/java/com/example/recipeproject/repositories/CategoryRepository.java
@@ -1,7 +1,11 @@
 package com.example.recipeproject.repositories;
 
+import java.util.Optional;
+
 import com.example.recipeproject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
