5268cd3e68c6563d179dd0697f8222cd5d404d30
JosephineAkello <josephineakello2030@gmail.com>
2020-07-01 15:19:52 +0300

    add mocks and mockitos

---
+++ src/test/java/com/example/recipeproject/service/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.example.recipeproject.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.example.recipeproject.domain.Recipe;
+import com.example.recipeproject.repositories.RecipeRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before("hhhh")
+    void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository)
+    }
+
+
+    @Test
+    public void getRecipes() throws Exception {
+
+        Recipe recipe =new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+
+        assertEquals(recipeSet.size(), 0);
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
