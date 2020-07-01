3b7c219448ab70efb03bcaccdf8f17092df701eb
JosephineAkello <josephineakello2030@gmail.com>
2020-07-01 22:14:32 +0300

    create tests using Spring Mock MVC

--- src/test/java/com/example/recipeproject/controllers/IndexControllersTest.java
+++ src/test/java/com/example/recipeproject/controllers/IndexControllersTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +26,7 @@ class IndexControllersTest {
 
     RecipeServiceImpl recipeService;
 
+
     @Mock
     Model model;
 
@@ -34,13 +38,24 @@ class IndexControllersTest {
        controllers= new IndexControllers(recipeService);
     }
 
+
+    @Test
+    void testMockMvc() throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controllers).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOK())
+                .andExpect(view().name("index"));
+
+    }
+
+
     @Test
     void getIndexPage() throws Exception{
 
         String viewName = controllers.getIndexPage(model);
 
         assertEquals("index", viewName);
-        when(recipeService.getRecipes()).thenReturn(recipeData);
+        when(recipeService.getRecipes()).thenReturn(model);
 
         verify(recipeService, times(1)).getRecipes();
         verify(model,times(1)).addAttribute(eq("recipe"), anySet());
