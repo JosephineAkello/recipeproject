b276989e7110fe12344f298151a6bb74e67ec0a6
JosephineAkello <josephineakello2030@gmail.com>
2020-07-01 20:51:42 +0300

    add tests for index controllers

---
+++ src/test/java/com/example/recipeproject/controllers/IndexControllersTest.java
@@ -0,0 +1,49 @@
+package com.example.recipeproject.controllers;
+
+import java.util.HashSet;
+
+import com.example.recipeproject.domain.Recipe;
+import com.example.recipeproject.repositories.RecipeRepository;
+import com.example.recipeproject.service.RecipeService;
+import com.example.recipeproject.service.RecipeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class IndexControllersTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    Model model;
+
+    IndexControllers controllers;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+       controllers= new IndexControllers(recipeService);
+    }
+
+    @Test
+    void getIndexPage() throws Exception{
+
+        String viewName = controllers.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        verify(recipeService, times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipe"), anySet());
+
+    }
+}
